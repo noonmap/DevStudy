b5766904b4b642e7083c950e6c05a075b2816639
JCW <codnjs8221@naver.com>
2023-07-11 00:35:26 +0900

    230711

---
+++ Algorithm/230711_BJ_9655_돌 게임_dp.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.awt.Point;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if( ( N & 1) == 0) System.out.println("CY");
+		else System.out.println("SK");
+	}
+	
+}
